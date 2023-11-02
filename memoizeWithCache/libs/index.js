1d008e26fb8dd020279994bcf24c8a3472670fef
yongwoo <wooleejaan@gmail.com>
2023-11-02 19:32:51 +0900

    feat: Add root js file that acts as an entry

---
+++ memoizeWithCache/libs/index.js
@@ -0,0 +1,36 @@
+import fetchTodo from "./apis/fetchTodo.api";
+import memoizePromiseFn from "./helpers/memoizePromiseFn.helper";
+import get from "./helpers/getElem.helper";
+
+(function () {
+  "use strict";
+
+  const $root = get("#root");
+
+  let cachedFetchTodos = memoizePromiseFn(fetchTodo);
+
+  const createElements = async () => {
+    /* jsx */
+    $root.innerHTML = /* html */ `
+      <button type="button" id="btn">call api!</button>
+      `;
+
+    const btn = get("#btn");
+
+    btn.addEventListener("click", async () => {
+      const response2 = await cachedFetchTodos(2);
+      console.log(response2); // Caching API calls by memoizing promises
+    });
+
+    /* load init state */
+    const response1 = await cachedFetchTodos(1);
+    console.log(response1);
+  };
+
+  const init = () => {
+    window.addEventListener("DOMContentLoaded", () => {
+      createElements();
+    });
+  };
+  init();
+})();
