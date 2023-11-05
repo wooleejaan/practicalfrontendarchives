b3ce27dbf2dd606a3b58c6d7c5f346bd485ffca9
yongwoo <wooleejaan@gmail.com>
2023-11-05 23:43:02 +0900

    feat: Add Counter from child_microapp

--- microFrontend/simpleMicroFE/parent-microapp/src/App.jsx
+++ microFrontend/simpleMicroFE/parent-microapp/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import { Counter } from "child_microapp/Counter";
+
 import "./index.css";
 
 const App = () => (
@@ -9,6 +11,7 @@ const App = () => (
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Empty CSS</div>
+    <Counter />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
