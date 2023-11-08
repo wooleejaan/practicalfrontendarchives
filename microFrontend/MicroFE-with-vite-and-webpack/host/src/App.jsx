ff7b12d4963099bff9f4faa687993eaec7645505
yongwoo <wooleejaan@gmail.com>
2023-11-08 23:49:07 +0900

    feat: Implement host app using vite module federation

---
+++ microFrontend/MicroFE-with-vite-and-webpack/host/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import Button from "remoteApp/Button";
+import useStore from "remoteApp/store";
+
+function App() {
+  const [count, setCount] = useStore();
+  return (
+    <div>
+      <h1>Host Application</h1>
+      <Button />
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
