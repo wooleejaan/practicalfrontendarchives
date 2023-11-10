50459df07a2e44a78a4bec4db60f20f7cf07b9af
yongwoo <wooleejaan@gmail.com>
2023-11-10 17:14:52 +0900

    feat: Add Form components

--- react-hook-form-with-zod/app/page.tsx
+++ react-hook-form-with-zod/app/page.tsx
@@ -1,9 +1,16 @@
-import FormWithoutReactHookForm from "@/components/form-without-rhf";
+// import FormWithoutReactHookForm from "@/components/form-without-rhf";
+// import FormWithReactHookForm from "@/components/form-with-rhf";
+// import FormWithReactHookFormAndZod from "@/components/form-with-rhf-and-zod";
+
+import FormWithReactHookFormAndZodAndServer from "@/components/components/form-with-rhf-and-zod-and-server";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <FormWithoutReactHookForm />
+      {/* <FormWithoutReactHookForm /> */}
+      {/* <FormWithReactHookForm /> */}
+      {/* <FormWithReactHookFormAndZod /> */}
+      <FormWithReactHookFormAndZodAndServer />
     </main>
   );
 }
