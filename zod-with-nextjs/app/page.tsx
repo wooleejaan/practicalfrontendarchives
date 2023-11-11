b715adc1e004a8e421e6f1126d7adc6bf061bf5e
yongwoo <wooleejaan@gmail.com>
2023-11-11 12:40:48 +0900

    feat: Add main page

---
+++ zod-with-nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+// import Product from "@/examples/api-requests/product";
+
+// import Cart from "@/examples/local-storage/cart";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <Product /> */}
+      {/* <Cart /> */}
+    </main>
+  );
+}
