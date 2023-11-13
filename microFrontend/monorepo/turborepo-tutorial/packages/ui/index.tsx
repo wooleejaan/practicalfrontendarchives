4b3cc65f061d9afb73e79878ac5dfa89dd1d3a0a
yongwoo <wooleejaan@gmail.com>
2023-11-13 15:08:08 +0900

    feat: Implement client router at (packages/ui/index.tsx, apps/movies/src/App.tsx)

--- microFrontend/monorepo/turborepo-tutorial/packages/ui/index.tsx
+++ microFrontend/monorepo/turborepo-tutorial/packages/ui/index.tsx
@@ -5,44 +5,105 @@ import {
   AppShell as MantineAppShell,
   Title,
   MantineProvider,
+  Navbar,
+  UnstyledButton,
+  Group,
+  Text,
 } from "@mantine/core";
+
+export type Route = {
+  element: React.FunctionComponent;
+  path: string;
+};
+
+export type NavLink = {
+  label: string;
+  path: string;
+};
+
+function MainLink({ label, path }: NavLink) {
+  return (
+    <Link to={path}>
+      <UnstyledButton
+        sx={(theme) => ({
+          display: "block",
+          width: "100%",
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color:
+            theme.colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+          "&:hover": {
+            backgroundColor:
+              theme.colorScheme === "dark"
+                ? theme.colors.dark[6]
+                : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Group>
+          <Text size="sm">{label}</Text>
+        </Group>
+      </UnstyledButton>
+    </Link>
+  );
+}
 
 export const AppShell: React.FunctionComponent<{
   title: string;
+  routes: Route[];
+  navLinks: NavLink[];
   colorScheme?: "light" | "dark";
-}> = ({ title, colorScheme }) => {
+}> = ({ title, colorScheme, routes, navLinks }) => {
   return (
-    <MantineProvider
-      theme={{
-        colorScheme,
-      }}
-      withGlobalStyles
-      withNormalizeCSS
-    >
-      <MantineAppShell
-        header={
-          <Header
-            height={60}
-            p="xs"
-            styles={(theme) => ({
-              main: {
-                backgroundColor:
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[8]
-                    : theme.colors.gray[0],
-              },
-            })}
-            sx={{
-              display: "flex",
-            }}
-          >
-            <Title>{title}</Title>
-          </Header>
-        }
-        padding="md"
+    <BrowserRouter>
+      <MantineProvider
+        theme={{
+          colorScheme,
+        }}
+        withGlobalStyles
+        withNormalizeCSS
       >
-        <div>hello</div>
-      </MantineAppShell>
-    </MantineProvider>
+        <MantineAppShell
+          navbar={
+            <Navbar width={{ base: 300 }} height={500} p="xs">
+              {navLinks.map((link) => (
+                <MainLink {...link} key={link.path} />
+              ))}
+            </Navbar>
+          }
+          header={
+            <Header
+              height={60}
+              p="xs"
+              styles={(theme) => ({
+                main: {
+                  backgroundColor:
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[8]
+                      : theme.colors.gray[0],
+                },
+              })}
+              sx={{
+                display: "flex",
+              }}
+            >
+              <Title>{title}</Title>
+            </Header>
+          }
+          padding="md"
+        >
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<route.element />}
+              />
+            ))}
+          </Routes>
+          <Outlet />
+        </MantineAppShell>
+      </MantineProvider>
+    </BrowserRouter>
   );
 };
