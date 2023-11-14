2324f0b28f0431f6b214558406003f59993aba81
yongwoo <wooleejaan@gmail.com>
2023-11-15 00:31:31 +0900

    feat: Add component & test

---
+++ testing-with-react/simple-testing/__tests__/components/Button/Button.test.tsx
@@ -0,0 +1,27 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+
+import Button from "@/components/Button";
+
+describe("<Button />", () => {
+  test("renders label correctly", () => {
+    const label = "Click";
+    const onClickMock = jest.fn();
+    render(<Button label={label} onClick={onClickMock} />);
+
+    const buttonElement = screen.getByText(label);
+
+    expect(buttonElement.textContent).toEqual(label);
+  });
+  test("calls onClick when clicked", () => {
+    const testId = "btn";
+    const label = "Click";
+    const onClickMock = jest.fn();
+
+    render(<Button label={label} onClick={onClickMock} />);
+
+    const buttonElement = screen.getByTestId(testId);
+
+    fireEvent.click(buttonElement);
+    expect(onClickMock).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ testing-with-react/simple-testing/__tests__/components/PostCard/PostCard.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import PostCard from "@/components/PostCard";
+
+test("renders post card correctly", () => {
+  const post = { id: 1, title: "Test Post", body: "Test Body" };
+  render(<PostCard post={post} />);
+
+  const titleElement = screen.getByText("Test Post");
+  const bodyElement = screen.getByText("Test Body");
+
+  expect(titleElement.textContent).toBe("Test Post");
+  expect(bodyElement.textContent).toBe("Test Body");
+});
